b3f07e0dede28c44da59293e9967e4ea1de7c25a
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-02-01 15:14:20 +0000

    stylisation de la page forum.html

--- client/app/factory/userProvider/userProvider.service.js
+++ client/app/factory/userProvider/userProvider.service.js
@@ -2,20 +2,37 @@
 const angular = require('angular');
 
 /*@ngInject*/
-export function userProviderService($http,$q) {
-	// AngularJS will instantiate a singleton by calling "new" on this function
-  this.getUsers=function () {
-    var deferred=$q.defer();
-    var liste=[];
-    $http.get('/api/sujets',{
+export function userProviderService($http, $q) {
+  // AngularJS will instantiate a singleton by calling "new" on this function
+  this.getUsers = function () {
+    var deferred = $q.defer();
+    var liste = [];
+    $http.get('/api/sujets', {
       cache: true
-    }).then(function(list) {
+    }).then(function (list) {
       //console.log("cycle",list);
-      liste=list.data;
+      liste = list.data;
       deferred.resolve(liste);
 
     });
-    liste=deferred.promise;
+    liste = deferred.promise;
+
+    return liste;
+
+  }
+
+  this.getUsersBySujet = function (sujet, createur) {
+    var deferred = $q.defer();
+    var liste = [];
+    $http.get('/api/messages/usersujet/'+sujet+'/' +  createur,  {
+      cache: true
+    }).then(function (list) {
+      console.log("userssssss", list);
+      liste = list.data;
+      deferred.resolve(liste);
+
+    });
+    liste = deferred.promise;
 
     return liste;
 
