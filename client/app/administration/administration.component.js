6159126919d8cd3d91d95793adb704bd1e5a4d4a
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-02-07 16:50:35 +0000

    ajout de la fonctionnalité modifier article

--- client/app/administration/administration.component.js
+++ client/app/administration/administration.component.js
@@ -8,86 +8,18 @@ import routes from './administration.routes';
 export class AdministrationComponent {
   /*@ngInject*/
   jsFunctionProvider;
-
-  constructor(jsFunctionProvider) {
-    this.jsFunctionProvider = jsFunctionProvider;
-    console.log('this', this);
-
-  }
-
-
-  Init() {
-
-    angular.element(document)
-      .ready(() => {
-
-        console.log('document pilier', document);
-        /* demo animated */
-        this.jsFunctionProvider.demoAnimated();
-
-        /* Document REady */
-        this.jsFunctionProvider.onDocumentReady();
-
-
-        /* on Page Load */
-        this.jsFunctionProvider.onPageLoad();
-        this.jsFunctionProvider.onPageResize();
-        this.jsFunctionProvider.onSliderArrowClick();
-        this.jsFunctionProvider.onPageScroll();
-        /*==============================*/
-        /* 08 - BUTTONS, CLICKS, HOVERS */
-        /*==============================*/
-        this.jsFunctionProvider.topMenu();
-        this.jsFunctionProvider.videoPlayBtn();
-        this.jsFunctionProvider.videoPlayBtnBig();
-        this.jsFunctionProvider.Popup();
-        this.jsFunctionProvider.hoverAnimation();
-        this.jsFunctionProvider.changeImageOnSpeaker();
-        this.jsFunctionProvider.hoverAnimationOnConference();
-        /*==================================================*/
-        /* 09 - TIMES, TABS */
-        /*==================================================*/
-        this.jsFunctionProvider.Timers();
-        this.jsFunctionProvider.CountDown();
-        this.jsFunctionProvider.Tabs();
-        /*=====================*/
-        /* 10 - LIGHT-BOX */
-        /*=====================*/
-        this.jsFunctionProvider.activityIndicatorFunctions();
-        this.jsFunctionProvider.closeButtonFunctions();
-        this.jsFunctionProvider.overLay();
-        this.jsFunctionProvider.capTion();
-        this.jsFunctionProvider.arrOws();
-        /*==================================================*/
-        /* 11 - STYLE BAR */
-        /*==================================================*/
-        this.jsFunctionProvider.confButton();
-        this.jsFunctionProvider.entryButton();
-
-        /*==================================================*/
-        /* 13 - AJAX CONTACT FORM */
-        /*==================================================*/
-        this.jsFunctionProvider.ajaxContactForm();
-
-
-
-
-      });
-
-
-    setTimeout(function () {
-      document.querySelector('header').style.backgroundColor = '#222';
-    }, 100);
-  }
-}
-export class AdministrationArticleComponent {
-  /*@ngInject*/
-  jsFunctionProvider;
   themeProvider;
-
-  constructor(jsFunctionProvider, themeProvider) {
+  articleProvider;
+  listArticles;
+  constructor(jsFunctionProvider, themeProvider, articleProvider) {
+    this.titre = '';
+    this.auteur = '';
+    this.contenu = '';
+    this.themearticl = '';
+    this.une = '';
     this.themeProvider = themeProvider;
     this.jsFunctionProvider = jsFunctionProvider;
+    this.articleProvider = articleProvider;
     console.log('this', this);
 
   }
@@ -106,6 +38,12 @@ export class AdministrationArticleComponent {
       this.listTheme = this.themeProvider.listTheme
       console.log('themes non vide', this.listTheme)
     }
+
+    this.articleProvider.listArticles().then(list => {
+      this.listArticles = list;
+
+      console.log('article', this.listArticles)
+    });
     angular.element(document)
       .ready(() => {
 
@@ -167,12 +105,22 @@ export class AdministrationArticleComponent {
       document.querySelector('header').style.backgroundColor = '#222';
     }, 100);
   }
+
+  editArticl(article) {
+    console.log('okkkkkk')
+    this.titre = article.titre;
+    this.auteur = article.auteur;
+    this.contenu = article.contenu;
+    this.themearticl = article.id_theme;
+    this.une = article.une;
+
+  }
 }
 
 
 
-AdministrationComponent.$inject = ["jsFunctionProvider"];
-AdministrationArticleComponent.$inject = ["jsFunctionProvider", "themeProvider"];
+
+AdministrationComponent.$inject = ["jsFunctionProvider", "themeProvider", "articleProvider"];
 export default angular.module('emergenceInsightsApp.administration', [uiRouter])
   .config(routes)
   .component('administration', {
@@ -182,7 +130,12 @@ export default angular.module('emergenceInsightsApp.administration', [uiRouter])
   })
   .component('editerarticle', {
     template: require('./editerarticle.html'),
-    controller: AdministrationArticleComponent,
+    controller: AdministrationComponent,
+    controllerAs: 'vm'
+  })
+  .component('editerinterviews', {
+    template: require('./editerinterviews.html'),
+    controller: AdministrationComponent,
     controllerAs: 'vm'
   })
   .name;
