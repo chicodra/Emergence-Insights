a8ed7b923c8460aa928082e951857c902c1198f9
pach93 <dergui2006@hotmail.com>
2017-02-08 10:01:54 +0000

    ajout dynamique des sous catégories dans la liste des sujets

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -38,6 +38,7 @@ export default class LoginController {
         .then(() => {
           // Logged in, redirect to home
           this.$state.go('main');
+          
         })
         .catch(err => {
           this.errors.login = err.message;
