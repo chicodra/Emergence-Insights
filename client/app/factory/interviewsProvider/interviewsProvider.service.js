321dc900889694130e501f3b7b67716c6ebbe5b0
sadioraymond <sadiowalyraymond@gmail.com>
2017-02-08 18:00:07 +0000

    Revert "Réglages Conflits"
    This reverts commit 4166971ea721d7fcf8e8ed5b65d1fc0606c1ff01.
    
    Conflicts:
    	client/app/administration/administration.component.js

--- client/app/factory/interviewsProvider/interviewsProvider.service.js
+++ client/app/factory/interviewsProvider/interviewsProvider.service.js
@@ -2,83 +2,54 @@
 const angular = require('angular');
 
 /*@ngInject*/
-export function interviewsProvider($http, $q) {
-  // AngularJS will instantiate a singleton by calling "new" on this function
-  this.listeInt = null;
-  this.listInterviews = function () {
-    var deferred = $q.defer();
-    var liste = [];
-    $http.get('/api/interviews', {
+export function interviewsProvider($http,$q) {
+	// AngularJS will instantiate a singleton by calling "new" on this function
+  this.listeInt=null;
+  this.listInterviews=function () {
+    var deferred=$q.defer();
+    var liste=[];
+    $http.get('/api/interviews',{
       cache: true
-    }).then(function (list) {
+    }).then(function(list) {
       //console.log("cycle",list);
-      liste = list.data;
+      liste=list.data;
       deferred.resolve(liste);
 
     });
-    liste = deferred.promise;
+    liste=deferred.promise;
 
     return liste;
 
   }
-  this.getInterviewByName = function (libelle) {
-    var deferred = $q.defer();
-    var liste = [];
-    $http.get('/api/interviews/themes/' + libelle, {
+  this.getInterviewByName=function (libelle) {
+    var deferred=$q.defer();
+    var liste=[];
+    $http.get('/api/interviews/themes/'+libelle,{
       cache: true
-    }).then(function (list) {
+    }).then(function(list) {
       //console.log("cycle",list);
-      liste = list.data;
+      liste=list.data;
       deferred.resolve(liste);
 
     });
-    liste = deferred.promise;
+    liste=deferred.promise;
 
     return liste;
 
   }
-  this.getNumberInterviews = function () {
-    var deferred = $q.defer();
-    var liste = [];
-    $http.get('/api/interviews/nombre/', {
+   this.getNumberInterviews=function () {
+    var deferred=$q.defer();
+    var liste=[];
+    $http.get('/api/interviews/nombre/',{
       cache: true
-    }).then(function (list) {
+    }).then(function(list) {
       //console.log("cycle",list);
-      liste = list.data;
+      liste=list.data;
       deferred.resolve(liste);
     });
-    liste = deferred.promise;
+    liste=deferred.promise;
     return liste;
 
-  }
-
-  this.ajoutInterview = function (libelle, intervenant, contenu, theme) {
-    var deferred = $q.defer();
-    $http.post('/api/interviews', {
-      libelle: libelle,
-      intervenant: intervenant,
-      contenu: contenu,
-      id_theme: theme
-    }).then(function () {
-      console.log("Interview bi Dougeuna");
-    });
-  }
-  this.modifier = function (id, libelle, intervenant, contenu, theme) {
-    var deferred = $q.defer();
-    $http.put('/api/interviews/' + id, {
-      libelle: libelle,
-      intervenant: intervenant,
-      contenu: contenu,
-      id_theme: theme
-    }).then(function () {
-      console.log("Modifier bi Bakhna");
-    });
-  }
-  this.supprimer = function (id) {
-    var deferred = $q.defer();
-    $http.delete('/api/interviews/' + id, {}).then(function () {
-      console.log("Supprimer bi Bakhna");
-    });
   }
 }
 
