b622762b842237c7e346afced3decfe8ac153608
Ahmadou Waly Ndiaye <ahmadouwalyndiaye@gmail.com>
2017-02-14 13:46:09 +0000

    *** ajout de l'animation du carré bleu au niveau de la page accueil *** Affichage des sous menu pilier et administration verticalement (navbar.html) *** Affichage des images de l'article et de l'interview au niveau de leur page specifique

--- client/app/article/article.component.js
+++ client/app/article/article.component.js
@@ -22,6 +22,9 @@ export class ArticleComponent {
       this.article = list[0];
       this.image = '../../assets/images/perfstock/article/' + this.article.image;
       console.log('image', this.image);
+      document.getElementById('imagArticle').style.backgroundImage = 'url(../../assets/images/perfstock/article/' + this.article.image+ ')';
+      document.getElementById('imagArticle').style.backgroundSize = 'cover';
+      console.log('imagArticle', imagArticle);
       console.log('single article', this.article)
     });
     this.jsFunctionProvider = jsFunctionProvider;
